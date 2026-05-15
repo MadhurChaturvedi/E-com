6745beb0b2477e3a8c28479d7d5ab7b5efca699f
madhur <madhurchaturvedi2000@gmail.com>
2026-05-15 15:54:52 +0530

    opt service done

--- server/controller/User.js
+++ server/controller/User.js
@@ -1,8 +1,30 @@
+import { OTP } from "../model/Otp.js";
+import sendotp from "../utils/sendOTp.js";
 import TryCatch from "../utils/TryCatch.js";
 
 export const loginUser = TryCatch(async (req, res) => {
   const { email } = req.body;
+  const subject = "Ecomerce App";
 
-  console.log(email);
-  res.send(email);
+  const otp = Math.floor(Math.random() * 1000000);
+
+  const prevOTP = await OTP.findOne({
+    email,
+  });
+
+  if (prevOTP) {
+    await prevOTP.deleteOne();
+  }
+
+  await sendotp({
+    email,
+    subject,
+    otp,
+  });
+
+  await OTP.create({ email, otp });
+
+  res.json({
+    message: "OTP send to you mail",
+  });
 });
